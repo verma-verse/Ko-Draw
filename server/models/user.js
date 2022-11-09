6f9dcdcc3d59c8730a30149998baed07cbbf2ad9
Ashish-Verma-MNNIT <ashishkv.2016@gmail.com>
2022-11-09 16:54:40 +0530

    working on nodemailer to send mails

--- server/models/user.js
+++ server/models/user.js
@@ -2,32 +2,33 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
-const JWT_PRIVATE_KEY = process.env.JWTPRIVATEKEY
+const JWT_PRIVATE_KEY = process.env.JWTPRIVATEKEY;
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    dp: { type: String },
+  isVerified: { type: Boolean, default: false },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  dp: { type: String },
 });
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, JWT_PRIVATE_KEY, {
-        expiresIn: "7d",
-    });
-    return token;
+  const token = jwt.sign({ _id: this._id }, JWT_PRIVATE_KEY, {
+    expiresIn: "7d",
+  });
+  return token;
 };
 
 const User = mongoose.model("user", userSchema);
 
 const validate = (data) => {
-    const schema = Joi.object({
-        firstName: Joi.string(),
-        lastName: Joi.string(),
-        email: Joi.string().email(),
-        password: passwordComplexity(),
-    });
-    return schema.validate(data);
+  const schema = Joi.object({
+    firstName: Joi.string(),
+    lastName: Joi.string(),
+    email: Joi.string().email(),
+    password: passwordComplexity(),
+  });
+  return schema.validate(data);
 };
 
 module.exports = { User, validate };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,73 +1,91 @@
 const router = require("express").Router();
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
-const Joi = require("joi");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const { EMAIL, PASSWORD } = process.env;
 
 router.post("/login", async (req, res) => {
-    try {
-        const { error } = validate(req.body);
-        if (error)
-            return res.status(400).send({ success: false, message: error.details[0].message });
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res
+        .status(400)
+        .send({ success: false, message: error.details[0].message });
 
-        const user = await User.findOne({ email: req.body.email });
-        if (!user)
-            return res.status(401).send({ message: "Invalid Email or Password" });
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(401).send({ message: "Invalid Email or Password" });
 
-        const validPassword = await bcrypt.compare(
-            req.body.password,
-            user.password
-        );
-        if (!validPassword)
-            return res.status(401).send({ message: "Invalid Email or Password" });
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(401).send({ message: "Invalid Email or Password" });
 
-        const token = user.generateAuthToken();
-        res.status(200).send({ data: token, message: "logged in successfully" });
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ message: "Internal Server Error" });
-    }
+    const token = user.generateAuthToken();
+    res.status(200).send({ data: token, message: "logged in successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 router.post("/register", async (req, res) => {
-    try {
-        console.log(req.body)
-        //TODO: validate the data (can be done in the frontend too.)
-        // const { error } = validate(req.body);
-        // if (error)
-        //     return res.status(400).send({ message: error.details[0].message });
+  const user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res.status(409).send({
+      success: false,
+      message: "User with given email already Exist!",
+    });
 
-        const user = await User.findOne({ email: req.body.email });
-        if (user)
-            return res
-                .status(409)
-                .send({ success: false, message: "User with given email already Exist!" });
+  const salt = await bcrypt.genSalt(Number(process.env.SALT));
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+  try {
+    const user = await new User({ ...req.body, password: hashPassword }).save();
+    const code = getrandomOTP();
+    const redirectURL = user._id.toString() + code;
+    let transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: EMAIL,
+        pass: PASSWORD,
+      },
+    });
 
-        const salt = await bcrypt.genSalt(Number(process.env.SALT));
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-        await new User({ ...req.body, password: hashPassword }).save();
-        res.status(201).send({ success: true, message: "User created successfully" });
-    } catch (error) {
-        res.status(500).send({ success: false, message: "Internal Server Error" });
-    }
+    transport.sendMail({
+      from: EMAIL,
+      to: req.body.email,
+      subject: "Please confirm your account",
+      html: `<h1>Email Confirmation</h1>
+                <h3>Hello ${req.body.firstName}</h3>
+                <p>Thank you for considering us. Please confirm your email by clicking on the following link</p>
+                <a href=http://localhost:8000/api/auth/verify/${redirectURL}> Click here</a>
+                </div>`,
+    });
+    res.json({
+      status: 201,
+      message: "We've just sent an email... verify your account",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: "Internal Server Error" });
+  }
 });
 
-
-const validate = (data) => {
-    const schema = Joi.object({
-        email: Joi.string().email().required().label("Email"),
-        password: Joi.string().required().label("Password"),
-    });
-    return schema.validate(data);
-};
-
+router.get("/vreify/:id", (req, res) => {
+  const code = req.params.id.slice(-6);
+  console.log(code);
+});
 const getrandomOTP = () => {
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let otp = ""
-    for (let i = 1; i <= 6; i++) {
-        otp += str[Math.floor(Math.random() * 36)]
-    }
-    return otp;
-}
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let otp = "";
+  for (let i = 1; i <= 6; i++) {
+    otp += str[Math.floor(Math.random() * 36)];
+  }
+  return otp;
+};
 
 module.exports = router;
